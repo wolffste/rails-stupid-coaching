d359eb1121146a2076be4c3a8fd12006ab75e7d7
Stefan Wolff <wolffste@gmail.com>
2020-02-17 15:16:47 +0100

    stupid

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
